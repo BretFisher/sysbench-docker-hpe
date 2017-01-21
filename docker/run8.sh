86c917085151736961b4d630bee78fa6a84aaad2
Bret Fisher <bret@fishbrains.com>
2017-01-21 01:33:40 -0500

    more inline documentation in scripts

--- docker/run8.sh
+++ docker/run8.sh
@@ -4,6 +4,13 @@
 
 # use '-v /local/path:/var/lib/mysql' to store db's on host
 
+# Note you can use the following ENV variables to control certain things at runtime
+# defaults are in Dockerfile
+# just add to docker run command (before the final sysbench image name)
+# (before db's are built)
+# `-e SYSBENCH_TABLE_COUNT=3`        # number of db tables to fill with records
+# `-e SYSBENCH_TABLE_SIZE=1000000`   # number of records to fill in each table
+
 # Examples of how to control resources
 # use '--cpuset-cpus 1-4' to limit container to the first 4 cpu's
 # use '--memory 31G' for example to limit container to 31GB
@@ -14,5 +21,5 @@ for i in {1..8}
 do
   docker stop -t 30 sysbench$i
   docker rm sysbench$i
-  docker run -v /var/local/docker_mounts/sysbench$i:/var/lib/mysql -e SYSBENCH_TABLE_SIZE=10000000 --storage-opt size=28G -d --name sysbench$i sysbench
+  docker run -v /var/local/docker_mounts/sysbench$i:/var/lib/mysql --storage-opt size=28G -d --name sysbench$i sysbench
 done
--- docker/test8.sh
+++ docker/test8.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# this will run 5 passes of sysbench on each of 8 containers (they must be started first)
+
+# Note you can use the following ENV variables to control certain things at test runtime
+# defaults are in Dockerfile
+# just add to docker exec command
+# `-e SYSBENCH_NUM_THREADS`    # number of total cpu threads sysbench will use per container
+# `-e SYSBENCH_MAX_TIME`       # quit sysbench test after X seconds
+# `-e SYSBENCH_MAX_REQUESTS`   # quit sysbench test after X SQL requests
+
 for c in {1..8}
 
 do 
-  docker exec -d sysbench$c /bin/bash -c "(for i in {1..4}; do /opt/sysbench_run.sh; done) > runlog.log"
+  docker exec -d sysbench$c /bin/bash -c "(for i in {1..5}; do /opt/sysbench_run.sh; done) > runlog.log"
 done
